197bcfdb5527ddccbd690d6f2fa8ff5bceba4360
psicomante <psicomante@gmail.com>
2015-03-12 15:42:18 +0100

    grunt file for configurabile theme and resumes

--- Gruntfile.js
+++ Gruntfile.js
@@ -1,11 +1,31 @@
 module.exports = function(grunt) {
 
+  var updateDefaults = function(defaults, override){
+    if (override.file){
+      defaults.file = override.file;
+    }
+
+    if (override.folder) {
+      defaults.folder = override.folder;
+    }
+
+    if (override.theme) {
+      defaults.theme = override.theme;
+    }
+  };
+
   // Read configuration from _config.yml
-  var configuration = grunt.file.readYAML('config.yml');
+  var configurationFile = grunt.file.readYAML('config.yml');
+
+  var config = {
+    file: configurationFile.resume.default.file,
+    folder: configurationFile.resume.default.folder,
+    theme: configurationFile.resume.default.theme
+  };
 
   grunt.initConfig({
 
-    config: configuration.resume,
+    config: config,
 
     copy: {
       main: {
@@ -13,17 +33,32 @@ module.exports = function(grunt) {
         dest: 'resume.json',
       },
     },
+
     shell: {
-      resume: {
+      serve: {
         command: 'resume serve'
-      }
+      },
+      pdf: {
+        command: 'resume export -t <%= config.theme %> resume.pdf'
+      },
+      html: {
+        command: 'resume export -t <%= config.theme %> resume.html'
+      },
+      md: {
+        command: 'resume export -t <%= config.theme %> resume.md'
+      },
     },
   });
 
   grunt.loadNpmTasks('grunt-contrib-copy');
   grunt.loadNpmTasks('grunt-shell');
 
-  grunt.registerTask('resume:serve', ['copy:main', 'shell:resume']);
-  grunt.registerTask('default', ['resume:serve']);
+  // copy resume.json and resume.jpg, serve the current theme (calls index.js)
+  grunt.registerTask('serve', ['copy:main', 'shell:serve']);
+  //
+  grunt.registerTask('pdf', ['shell:pdf']);
+  grunt.registerTask('html', ['shell:html']);
+  grunt.registerTask('md', ['shell:md']);
+  grunt.registerTask('default', ['serve']);
 
 };
